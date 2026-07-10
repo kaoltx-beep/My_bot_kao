cc05e685cbad07c527334c2efc626c1fd514be22
kaoltx-beep <kao.ltx@gmail.com>
2026-07-10 20:34:23 +0700

    Add youtube plugin metadata

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,6 +1,11 @@
-PLUGIN_NAME = "open_youtube"
+METADATA = {
+    "name": "open_youtube",
+    "description": "เปิด YouTube บนอุปกรณ์ Android",
+    "category": "device",
+    "parameters": ["url", "search_query"]
+}
 
 
-def execute():
+def execute(args=None):
     import device_actions
     return device_actions.open_youtube()
