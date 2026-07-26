c0aaab509dceae4a33656ab1116597e53cfac6ec
Jarvis Test <test@jarvis.bot>
2026-07-26 07:58:35 +0700

    Jarvis update

--- plugins/hello.py
+++ plugins/hello.py
@@ -1,9 +1,2 @@
 def execute_hello(command):
-    if command.lower() == 'สวัสดีจาร์วิส':
-        return 'สวัสดี นายท่าน'
-    else:
-        if command.lower().startswith('สวัสดี'):
-            var = command.lower()
-            var = var[5:]
-            return 'สวัสดี ' + var.upper() + ' นายท่าน'
-    return ''
+    return 'สวัสดี นายท่าน'
