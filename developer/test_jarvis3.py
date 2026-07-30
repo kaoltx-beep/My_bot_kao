ffe628e03d96bb0b42ec044bc4b883bc0ba3e125
Jarvis Test <test@jarvis.bot>
2026-07-30 18:50:09 +0700

    Jarvis: สร้างไฟล์ developer/test_jarvis3.py

---
+++ developer/test_jarvis3.py
@@ -0,0 +1,9 @@
+def test_jarvis3():
+    def add(a, b):
+        return a + b
+
+    def subtract(a, b):
+        return a - b
+
+    print("ผลต่างระหว่าง 10 และ 5 คือ: ", subtract(10, 5))
+    print("ผลรวมของ 10 และ 5 คือ: ", add(10, 5))
